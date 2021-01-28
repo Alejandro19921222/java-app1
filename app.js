e46388fed9a84ba8d4d1f8bbe1d1e2dcbe36880d
丹尾州満 <s.nio@tannooshuumannoMacBook-Air.local>
2021-01-28 13:31:40 +0900

    [finish]

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-let buttonList = document.querySelectorAll("button");
-[...buttonList].forEach((button) => {
-  let li = button.closest("li");
-  button.addEventListener("click",() => {    
-    li.classList.add("done");
-  });
-});
-// let li = document.querySelector("li");
+let button = document.getElementById("add-button");
+  button.addEventListener("click", () => {
+    let input = document.getElementById("new-todo");
+    
+    let li = document.createElement("li");
+    li.innerText = input.value;
 
+    let doneButton = document.createElement("button")
+    doneButton.innerText = "done";
+    li.appendChild(doneButton);
+    doneButton.addEventListener("click", () => {
+      doneButton.closest("li");
+      li.classList.add("done");
+    });
+
+    let ul = document.querySelector("ul");
+    ul.appendChild(li);
+});
+
+
+
+// let li = document.querySelector("li");
